b351de4200a0864dfe2ed027f10c00b226bbabc7
davifdepaula <davi.ferreira100@gmail.com>
2023-01-04 15:00:25 -0300

    fix: correct status code e string type

--- src/middleware/validateDateMiddleWare.js
+++ src/middleware/validateDateMiddleWare.js
@@ -2,7 +2,9 @@ import { users } from "../router.js"
 
 const validateBodySign = (req, res, next) => {
     if(!req.body.username) return res.status(400).send("Todos os campos são obrigatórios!")
+    if(typeof req.body.username !== "string") return res.status(400).send("O nome de usuário precisa ser uma string")
     if(!req.body.avatar) return res.status(400).send("Todos os campos são obrigatórios!")
+    if(typeof req.body.avatar !== "string") return res.status(400).send("O avatar de usuário precisa ser uma string")
     next()
 }
 
@@ -13,10 +15,11 @@ const validateBodyTweets = (req, res, next) => {
         element.username === req.body.username
     )
     if(!isLogged) {
-        return res.status(401).send("UNAUTHORIZED")
+        return res.status(200).send("UNAUTHORIZED")
     }
 
     if(!req.body.tweet) return res.status(400).send("Todos os campos são obrigatórios!")
+    if(typeof req.body.tweet !== "string") return res.status(400).send("O tweet precisa ser uma string")
     next()
 }
 
